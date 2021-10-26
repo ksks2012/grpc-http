f20f644ff3865f430fc6c57e20a1ba016f771760
ksks2012 <st890751999@gmail.com>
2021-10-27 04:39:52 +0800

    feat(tag-service): reuse code in tag service

---
+++ server/tag.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"context"
+	"encoding/json"
+
+	// "net/http"
+
+	bapi "github.com/grpc-http/pkg/bapi"
+	"github.com/grpc-http/pkg/errcode"
+	pb "github.com/grpc-http/proto"
+)
+
+type TagServer struct{}
+
+func NewTagServer() *TagServer {
+	return &TagServer{}
+}
+
+func (t *TagServer) GetTagList(ctx context.Context, r *pb.GetTagListRequest) (*pb.GetTagListReply, error) {
+	api := bapi.NewAPI("http://127.0.0.1:18080/api/v1/tags?name=" + r.GetName())
+	body, err := api.GetTagList(ctx, r.GetName())
+	if err != nil {
+		return nil, errcode.TogRPCError(errcode.ErrorGetTagListFail)
+	}
+
+	// resp, err := http.Get("http://127.0.0.1:18080/api/v1/tags?name=" + r.GetName())
+	// if err != nil {
+	// 	return nil, errcode.TogRPCError(errcode.ErrorGetTagListFail)
+	// }
+
+	tagList := pb.GetTagListReply{}
+	err = json.Unmarshal(body, &tagList)
+	if err != nil {
+		return nil, errcode.TogRPCError(errcode.Fail)
+	}
+
+	return &tagList, nil
+}
