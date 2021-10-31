e633532524c9c6be64f3a6fbac3421fd65450bd0
ksks2012 <st890751999@gmail.com>
2021-10-31 21:51:16 +0800

    feat(error code): customize
    customize http error code

--- cmd/grpc-http/main.go
+++ cmd/grpc-http/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"log"
 	"net"
@@ -16,7 +17,9 @@ import (
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 var port string
@@ -34,6 +37,30 @@ func main() {
 
 }
 
+type httpError struct {
+	Code    int32  `json:"code,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+func grpcGatewayError(ctx context.Context, _ *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, _ *http.Request, err error) {
+	s, ok := status.FromError(err)
+	if !ok {
+		s = status.New(codes.Unknown, err.Error())
+	}
+	httpError := httpError{Code: int32(s.Code()), Message: s.Message()}
+	details := s.Details()
+	for _, detail := range details {
+		if v, ok := detail.(*pb.Error); ok {
+			httpError.Code = v.Code
+			httpError.Message = v.Message
+		}
+	}
+	resp, _ := json.Marshal(httpError)
+	w.Header().Set("Content-type", marshaler.ContentType())
+	w.WriteHeader(runtime.HTTPStatusFromCode(s.Code()))
+	_, _ = w.Write(resp)
+}
+
 func RunServer(port string) error {
 	httpMux := RunHttpServer()
 	grpcS := RunGrpcServer()
@@ -45,6 +72,8 @@ func RunServer(port string) error {
 }
 
 func RunGrpcGatewayServer() *runtime.ServeMux {
+	runtime.HTTPError = grpcGatewayError
+
 	endpoint := "0.0.0.0:" + port
 	gwmux := runtime.NewServeMux()
 	dopts := []grpc.DialOption{grpc.WithInsecure()}
