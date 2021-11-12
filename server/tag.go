aa02106c64f7f8038f137555a7c4fdc19bb996f2
ksks2012 <st890751999@gmail.com>
2021-11-13 02:44:04 +0800

    feat(server): add metadata checker for server

--- server/tag.go
+++ server/tag.go
@@ -9,10 +9,41 @@ import (
 	bapi "github.com/grpc-http/pkg/bapi"
 	"github.com/grpc-http/pkg/errcode"
 	pb "github.com/grpc-http/proto"
+	"google.golang.org/grpc/metadata"
 )
 
 type TagServer struct {
 	pb.TagServiceServer
+	auth *Auth
+}
+
+type Auth struct{}
+
+func (a *Auth) GetAppKey() string {
+	// TODO: get from DB
+	return "hong"
+}
+
+func (a *Auth) GetAppSecret() string {
+	// TODO: get from DB
+	return "blog-service"
+}
+
+func (a *Auth) Check(ctx context.Context) error {
+	md, _ := metadata.FromIncomingContext(ctx)
+
+	var appKey, appSecret string
+	if value, ok := md["app_key"]; ok {
+		appKey = value[0]
+	}
+	if value, ok := md["app_secret"]; ok {
+		appSecret = value[0]
+	}
+	if appKey != a.GetAppKey() || appSecret != a.GetAppSecret() {
+		return errcode.TogRPCError(errcode.Unauthorized)
+	}
+
+	return nil
 }
 
 func NewTagServer() *TagServer {
@@ -20,6 +51,9 @@ func NewTagServer() *TagServer {
 }
 
 func (t *TagServer) GetTagList(ctx context.Context, r *pb.GetTagListRequest) (*pb.GetTagListReply, error) {
+	if err := t.auth.Check(ctx); err != nil {
+		return nil, err
+	}
 	api := bapi.NewAPI("http://127.0.0.1:18080")
 	body, err := api.GetTagList(ctx, r.GetName())
 	if err != nil {
